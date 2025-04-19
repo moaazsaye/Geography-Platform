57368acc7181af577b0b1ab4b45cb0edadcb7ba8
moaazsaye <sayedmoaaz67@gmail.com>
2025-04-19 16:40:51 +0200

    Create script.js

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="style.css">document.addEventListener('DOMContentLoaded', () => {
+  alert("مرحباً بكم في منصة الجغرافي معاذ السيد!");
+});body {
+  background-color: #f0f0f0;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  padding: 20px;
+}
+
+h1 {
+  color: #333;
+}
